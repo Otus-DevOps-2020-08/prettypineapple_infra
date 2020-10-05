7e531526cb6461f20b3e5907a9c525988f39ab7c
Anna Vekhteva <gnedenko.anna@gmail.com>
2020-10-05 23:12:37 +0300

    Add module app

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,10 +1,12 @@
-resource "google_compute_instance" "app" {
+esource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
-  zone = var.zone
+  zone = var.instance_zone
   tags = ["reddit-app"]
   boot_disk {
-    initialize_params { image = var.app_disk_image }
+    initialize_params {
+        image = var.app_disk_image
+        }
   }
   network_interface {
     network = "default"
@@ -12,7 +14,7 @@ resource "google_compute_instance" "app" {
       nat_ip = google_compute_address.app_ip.address
     }
   }
-  metadata {
+  metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 }
@@ -25,7 +27,8 @@ resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
   network = "default"
   allow {
-    protocol = "tcp", ports = ["9292"]
+    protocol = "tcp"
+    ports = ["9292"]
   }
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["reddit-app"]
