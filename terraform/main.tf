df3d47314c1b9777cb2adcdfe3d7fecd90fa0a99
Anna Vekhteva <gnedenko.anna@gmail.com>
2020-10-05 23:13:20 +0300

    Add work with modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,75 +11,16 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = var.instance_zone
-  tags         = ["reddit-app"]
-
-  boot_disk {
-    initialize_params {
-      image = var.disk_image
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {nat_ip = google_compute_address.app_ip.address}
-  }
-
-  metadata = {
-    # путь до публичного ключа
-    ssh-keys = "appuser:${file(var.public_key_path)} \nappuser1:${file(var.public_key_path)} \nappuser2:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type  = "ssh"
-    host  = self.network_interface[0].access_config[0].nat_ip
-    user  = "appuser"
-    agent = false
-    # путь до приватного ключа
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-
+module "app" {
+  source          = "modules/app"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  app_disk_image  = var.app_disk_image
 }
 
-resource "google_compute_address" "app_ip" {
-  name = "reddit-app-ip"
-}
-
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-  # Название сети, в которой действует правило
-  network = "default"
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
-}
-
-resource "google_compute_firewall" "firewall_ssh" {
-  name = "default-allow-ssh"
-  network = "default"
-  description = "Allow SSH with terraform, not gcp"
-  allow {
-    protocol = "tcp"
-    ports = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
+module "db" {
+  source          = "modules/db"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  db_disk_image   = var.db_disk_image
 }
