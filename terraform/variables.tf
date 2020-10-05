94c4b1bdea52998ee32c5df943591b4df5555f48
Anna Vekhteva <gnedenko.anna@gmail.com>
2020-10-05 22:40:21 +0300

    Add db_disk_image variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,3 +22,7 @@ variable private_key_path {
 variable disk_image {
   description = "Disk image"
 }
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
