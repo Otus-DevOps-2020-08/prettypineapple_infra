c2827f124ac0f9915d1f2a27667fc665c92f4b73
Anna Vekhteva <gnedenko.anna@gmail.com>
2020-10-13 22:08:15 +0300

    Add stage and prod directories

---
+++ terraform/stage/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  # Версия terraform
+  required_version = "0.12.29"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.15"
+  # ID проекта
+  project = var.project
+  region  = var.region
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+}
+
+module "vpc" {
+  source          = "../modules/vpc"
+}
