7f47c16bb31df708336a279565c630585932a9c2
Anna Vekhteva <gnedenko.anna@gmail.com>
2020-11-19 17:49:45 +0300

    Expose 80 instead of 9292

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -23,12 +23,12 @@ resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
 }
 
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+resource "google_compute_firewall" "firewall_puma_nginx" {
+  name = "allow-puma-nginx"
   network = "default"
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports = ["80"]
   }
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["reddit-app"]
